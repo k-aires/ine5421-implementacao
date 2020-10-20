09b19113b1eac370ad22c6872b3465a325b55ea6
Helena Aires <lenakaires@gmail.com>
2020-10-20 18:40:18 -0300

    Adicionando salvamento de estruturas

--- manip/__init__.py
+++ manip/__init__.py
@@ -4,6 +4,18 @@ import expression
 import grammar
 import automata
 
+import json
+import os
+from enum import Enum, auto
+
+class StructType(Enum):
+    AUTOMATA = auto()
+    EXPRESSION = auto()
+    GRAMMAR = auto()
+
+struct = {}
+struct_type = 0
+
 def menu():
     # Chama interface do menu
     inp = io_terminal.initial_menu()
@@ -28,19 +40,75 @@ def input_menu():
         menu()
 
 def file_menu(opening):
+    global struct_type
+    global struct
+
+    file_path = ".save/"
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
     if opening:
-        io_terminal.open_file_menu()
+        file_name = io_terminal.open_file_menu()
     else:
-        io_terminal.save_file_menu()
+        file_name = io_terminal.save_file_menu()
+        with open(file_path+file_name,"w") as f:
+            print(json.dumps([struct_type.value,struct]),file=f)
+        menu()
 
 def finite_automata():
-    aut = io_terminal.automata_input()
+    global struct_type
+    global struct
+    struct_type = StructType.AUTOMATA
+    struct = io_terminal.automata_input()
+    inp = io_terminal.finite_automata_menu()
+    if inp == 0: # Conversão para AFD
+        io_terminal._nope()
+    elif inp == 1: # Conversão para GR
+        io_terminal._nope()
+    elif inp == 2: # Reconhecimento de sentença
+        io_terminal._nope()
+    elif inp == 3: # Minimização
+        io_terminal._nope()
+    elif inp == 4: # União
+        io_terminal._nope()
+    elif inp == 5: # Interseção
+        io_terminal._nope()
+    elif inp == 6: # Editar
+        io_terminal._nope()
+    elif inp == 7: # Salvar
+        file_menu(False)
+    elif inp == 8: # Menu
+        menu()
 
 def regular_grammar():
-    gram = io_terminal.grammar_input(0)
+    global struct_type
+    global struct
+    struct_type = StructType.GRAMMAR
+    struct = io_terminal.grammar_input(0)
+    inp = io_terminal.regular_grammar_menu()
+    if inp == 0: # Conversão para AFND
+        io_terminal._nope()
+    elif inp == 1: # Editar
+        io_terminal._nope()
+    elif inp == 2: # Salvar
+        file_menu(False)
+    elif inp == 2: # Menu
+        menu()
 
 def regular_expression():
-    exp = io_terminal.expression_input()
+    global struct_type
+    global struct
+    struct_type = StructType.EXPRESSION
+    struct = io_terminal.expression_input()
+    inp = io_terminal.regular_expression_menu()
+    if inp == 0: # Conversão para AFD
+        io_terminal._nope()
+    elif inp == 1: # Editar
+        io_terminal._nope()
+    elif inp == 2: # Salvar
+        file_menu(False)
+    elif inp == 2: # Menu
+        menu()
 
 def context_free_grammar():
     io_terminal._nope()
